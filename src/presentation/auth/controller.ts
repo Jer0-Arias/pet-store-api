5580738d0f69a01d97bb3545f7efa4194266181e
ariasmosquerajeronimo <ariasmosquerajeronimo@gmail.com>
2024-06-18 10:40:32 +0000

    Implementación logica de consumo del AuthRepository”

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,19 +1,24 @@
 import { Request, Response } from 'express'; // 5
+import { RegisterUserDto, AuthRepository } from './../../domain'; // 1
+
+
+
 
 export class AuthController { // 1
 
-    constructor(){} // 2
+    constructor(
+        private readonly authRepository: AuthRepository //9
+    ){} // 2
 
-    registerUser = (req: Request, res: Response ) => {
+    registerUser = (req: Request, res: Response) => { // 4
         const [error, registerUserDto] = RegisterUserDto.create(req.body);
         if ( error ) return res.status(400).json({ error });
-        
-        res.json(registerUserDto)
-      }
+
+        res.json(registerUserDto) //10
+    } // 3
 
     loginUser =  (req: Request, res: Response) => { // 7
         res.json('loginUser controller') // 8
-
     } // 6
 
 }
