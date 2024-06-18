c24f2582a4624b139b5193564a6313e26db04664
ariasmosquerajeronimo <ariasmosquerajeronimo@gmail.com>
2024-06-18 11:45:31 +0000

    creacion de usuario en base de datos mongoDB

--- src/infraestructure/datasources/auth.datasource.impl.ts
+++ src/infraestructure/datasources/auth.datasource.impl.ts
@@ -1,27 +1,38 @@
+import { UserModel } from "../../data/models/users.model";
 import { AuthDataSource, CustomError, RegisterUserDto, UserEntity } from "../../domain"; //2
 
 
 
 export class AuthDataSourceImpl implements AuthDataSource {//1
     async register(registerUserDto: RegisterUserDto): Promise<UserEntity> {//3
-        const {name, email, password} = registerUserDto;// 4
+        const { name, email, password } = registerUserDto;// 4
 
-        try{//5
+        try {//5
 
+            const exists = await UserModel.findOne({ email });
+            if (exists) throw CustomError.badRequest("User already exists");
+
+            const user = await UserModel.create({
+                name: name,
+                email: email,
+                password: password,
+            });
+
+            await user.save();
 
             return new UserEntity(// 6
-                '1', 
-                name, 
-                email, 
-                password, 
-                ['USER']);    
+                user.id,
+                name,
+                email,
+                password,
+                user.roles);
 
-        }catch(error){
-            if(error instanceof CustomError){//7
+        } catch (error) {
+            if (error instanceof CustomError) {//7
                 throw error;
             }
             throw CustomError.internalServer();//8
         }
     }
-    
+
 }
